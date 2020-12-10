e14882d9425eafc11ce1ef8a1080ddf7101f7934
Yasmine <yasszmine@192.168.1.6>
2020-12-10 15:25:43 +0100

    left sidebar styling

--- src/components/LeftSidebar/LeftSidebar.js
+++ src/components/LeftSidebar/LeftSidebar.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './LeftSidebar.css'
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import './LeftSidebar.scss'
+import logo from '../../assets/logo.png';
 
 function LeftSidebar() {
+  const history = useHistory();
  return (
 <nav>
   <div className="upper">
-    <img src="" alt="logo" />
-    <h1>yasmine</h1>
+    <img 
+      src={logo} 
+      alt="logo" 
+      className="logo"
+      onClick={() => { history.push('/'); }}
+      />
+    <h1><span>yasmine abdelkefi</span></h1>
   </div>
   <div className="lower">
     <ul>
